fac4443f3205b74b263cf1e1e0fcd676e9a3baad
Sithum-Perera <167444560+Sithum-Perera@users.noreply.github.com>
2025-09-22 13:34:29 +0530

    move to checkout page to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePage'
+import { CheckOutPage } from './pages/CheckOutPage'
 
 function App() {
   return (
     <Routes>
-      <Route index element={<HomePage/>} />
-      <Route path="checkout" element={<div>test</div>} />
+      <Route path='/' element={<HomePage/>} />
+      <Route path="/checkout" element={<CheckOutPage/>} />
     </Routes>
   )
 }
